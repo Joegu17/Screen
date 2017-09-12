1f52924b8ea468c54828d6975a5cf2525693ff9e
Joegu17 <joel@familiehari.ch>
2017-09-12 18:32:32 +0200

    Delete javascript.js

--- javascript.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-document.addEventListener('DOMContentLoaded',messen);
-document.addEventListener('resize',messen);
-
-function messen() {
-	document.getElementById('clientW').textContent = document.querySelector('html').clientWidth;
-	document.getElementById('clientH').textContent = document.querySelector('html').clientHeight;
-}
