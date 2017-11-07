8b625fae2f316cebfa4c787e82746e888a3d6e3a
Joegu17 <joel@familiehari.ch>
2017-11-07 20:23:01 +0100

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -9,40 +9,44 @@ $('.hintergrund').css({width: w+'px', height: h+'px'});
 $('#klotz').css({width: h*3/18/242*353+'px', height: h*3/18+'px'});
 $('#level1').css({width: h*10+'px', height: h+'px'});
 
-$('#level1').css('transform', 'translate3d('+(-h*9)+'px, 0px, 0px)');
+
 
 function loop(direction) {
     
     if (direction == 1) {
         
-        $('#klotz').css('transform', 'translate3d('+(-w-h*3/18/242*353)+'px, 0px, 0px)');
+        $('#level1').css('transform', 'translate3d('+(-h*10+w)+'px, 0px, 0px)');
+        
+        //$('#klotz').css('transform', 'translate3d('+(-w-h*3/18/242*353)+'px, 0px, 0px)');
         
         //$('#bild').css('background-image', 'url(../images/banger_ball.bmp)');
         
         //$('#bild').css({'background-image': 'url(../images/banger_ball.bmp)'});
         
-        $('#bild1').css({display: 'none'});
+        //$('#bild1').css({display: 'none'});
         
-        $('#bild2').css({display: 'inherit'});
+        //$('#bild2').css({display: 'inherit'});
         
     } else {
         
-        $('#klotz').css('transform', 'none');
+        $('#level1').css('transform', 'none');
+        
+        //$('#klotz').css('transform', 'none');
         
         //$('#bild').css('background-image', 'url(../images/beach_ball.png)');
         
         //$('#bild').css({'background-image': 'url(../images/beach_ball.png)'});
         
-        $('#bild1').css({display: 'inherit'});
+        //$('#bild1').css({display: 'inherit'});
         
-        $('#bild2').css({display: 'none'});
+        //$('#bild2').css({display: 'none'});
         
     }
     
     d = d * (-1);
     
-    window.setTimeout(loop, 3100, d);
+    window.setTimeout(loop, 30100, d);
     
 }
 
-//loop(d);
+loop(d);
