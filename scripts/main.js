4d51a5a15dc3267e616ae3437e4a640cdb58b835
Joegu17 <joel@familiehari.ch>
2017-11-08 10:00:07 +0100

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -7,7 +7,6 @@ var w = screen.width,
 
 $('.hintergrund').css({width: w+'px', height: h+'px'});
 $('#klotz').css({width: h*3/18/242*353+'px', height: h*3/18+'px'});
-$('#level1').css({width: h*10+'px', height: h+'px'});
 
 
 
@@ -15,10 +14,7 @@ function loop() {
     
     if (d == 1) {
         
-        $('#level1').css('transform', 'translate3d('+(-h*10+w)+'px, 0px, 0px)');
-        
-        
-        //$('#klotz').css('transform', 'translate3d('+(-w-h*3/18/242*353)+'px, 0px, 0px)');
+        $('#klotz').css('transform', 'translate3d('+(-w-h*3/18/242*353)+'px, 0px, 0px)');
         
         //$('#bild').css('background-image', 'url(../images/banger_ball.bmp)');
         
@@ -30,11 +26,7 @@ function loop() {
         
     } else {
         
-        $('#level1').css('transform', 'none');
-        
-        
-        
-        //$('#klotz').css('transform', 'none');
+        $('#klotz').css('transform', 'none');
         
         //$('#bild').css('background-image', 'url(../images/beach_ball.png)');
         
@@ -48,7 +40,7 @@ function loop() {
     
     d = d * (-1);
     
-    window.setTimeout(loop, 26000);
+    window.setTimeout(loop, 3000);
     
 }
 
